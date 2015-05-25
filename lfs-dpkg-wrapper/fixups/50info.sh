4947d8d43a3d1359731418a74eccf13629fbbe6b
Daniel Schepler <dschepler@gmail.com>
2015-05-25 09:14:16 -0700

    Add lfs-dpkg-wrapper, lfs-base build instructions Add /tools/sbin to initial PATH to satisfy dpkg Bump versions Fix some fixup snippet bugs revealed by lfs-dpkg-wrapper, lfs-base builds

--- lfs-dpkg-wrapper/fixups/50info.sh
+++ lfs-dpkg-wrapper/fixups/50info.sh
@@ -4,7 +4,10 @@ fi
 
 if [ -d $packagedir/usr/share/info ]; then
     for f in $packagedir/usr/share/info/*.info; do
-	doit "echo install-info /usr/share/info/$f --dir-file=/usr/share/info/dir >> $packagedir/DEBIAN/postinst"
-	doit "echo install-info --delete /usr/share/info/$f --dir-file=/usr/share/info/dir >> $packagedir/DEBIAN/prerm"
+        b=$(basename $f)
+        if [ "$b" != "*.info" ]; then
+	    doit "echo install-info /usr/share/info/$b --dir-file=/usr/share/info/dir >> $packagedir/DEBIAN/postinst"
+	    doit "echo install-info --delete /usr/share/info/$b --dir-file=/usr/share/info/dir >> $packagedir/DEBIAN/prerm"
+        fi
     done
 fi
--- lfs-dpkg-wrapper/fixups/50strip.sh
+++ lfs-dpkg-wrapper/fixups/50strip.sh
@@ -1,7 +1,7 @@
 find $packagedir -type f |
     while read f; do
 	case "$(file --brief $f)" in
-	    ELF*executable*,\ not\ stripped |
+	    ELF*executable*,\ not\ stripped | \
 	    ELF*shared\ object*,\ not\ stripped)
 		doit "strip --strip-unneeded $f" ;;
 	    current\ ar\ archive)
